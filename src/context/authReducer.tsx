f47a82213484965bc6ff1c21beb03125b0007bdb
Akhilender Bongirwar <akhilenderb9@gmail.com>
2025-03-05 11:41:14 +0530

    fix: add auth reducers
    - Adding Reducers for the context api logic
    - action types of login and register
    
    Signed-off-by: Akhilender Bongirwar <akhilenderb9@gmail.com>

---
+++ src/context/authReducer.tsx
@@ -0,0 +1,29 @@
+import { AuthState, AuthAction } from './types';
+
+export const initialState: AuthState = {
+  is_connected: false,
+  account: '',
+  instance: null,
+  is_admin: false,
+  flag: false,
+};
+
+export const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case 'login':
+      return {
+        ...state,
+        is_connected: true,
+        account: action.payload?.account || '',
+        instance: action.payload?.instance || null,
+        is_admin: action.payload?.is_admin || false,
+        flag: action.payload?.flag || false,
+      };
+
+    case 'register':
+      return { ...state, flag: false };
+
+    default:
+      return state;
+  }
+};
