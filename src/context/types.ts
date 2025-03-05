9110d10f5df3ad938e53fb626d8eeb0b4fd74d2f
Akhilender Bongirwar <akhilenderb9@gmail.com>
2025-03-05 11:36:34 +0530

    feat: add context api
    - reducers for auth
    - added the types.ts
    
    Signed-off-by: Akhilender Bongirwar <akhilenderb9@gmail.com>

---
+++ src/context/types.ts
@@ -0,0 +1,23 @@
+import { Contract } from 'ethers';
+
+export interface AuthState {
+  is_connected: boolean;
+  account: string;
+  instance: Contract | null;
+  is_admin: boolean;
+  flag: boolean;
+}
+
+export interface AuthAction {
+  type: 'login' | 'register';
+  payload?: Partial<AuthState>;
+}
+
+export interface AuthContextProps {
+  state: AuthState;
+  dispatch: React.Dispatch<AuthAction>;
+}
+
+export interface AuthProviderProps {
+  children: React.ReactNode;
+}
